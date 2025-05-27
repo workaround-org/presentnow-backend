24b987ef7ff0b0a9ac5e498d8e9ee22b7d62aa25
Miggi <noreply@mymiggi.de>
2025-05-27 17:54:19 +0200

    🍱 Add demo data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,29 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+--
+-- PresentIdea
+--
+INSERT INTO public.presentidea (importance, id, listid, description, name, url)
+VALUES (1, 1, 1, 'A handcrafted star map of the night sky on a specific birthday date.', 'Personalized Star Map',
+        'https://example.com/star-map'),
+       (5, 2, 1, 'Premium over-ear headphones for immersive audio experiences.', 'Wireless Noise-Canceling Headphones',
+        'https://example.com/headphones'),
+       (4, 3, 1, 'A sleek smartwatch that monitors heart rate, sleep, and fitness goals.',
+        'Smartwatch with Health Tracking', 'https://example.com/smartwatch'),
+       (3, 4, 1, 'A gourmet cooking experience at a local restaurant.', 'Cooking Class for Two',
+        'https://example.com/cooking-class'),
+       (2, 5, 2, 'A modern desk lamp with built-in wireless charging and adjustable brightness.',
+        'Wireless Charging Desk Lamp', 'https://example.com/lamp');
+--
+--  WishList Data
+--
+INSERT INTO public.wishlist (active, expires, id, description, name, username)
+VALUES ('t', 1779888096, 1,
+        'A list of unforgettable experiences and adventures desired for the birthday celebration, such as travel vouchers, workshops, or special events!',
+        'Birthday Experience Wishlist', 'dummyUser'),
+       ('t', 1779888096, 2,
+        'A collection of thoughtful gift ideas and presents desired for the birthday celebration, including both tangible items and memorable experiences!',
+        'Birthday Gift Wishlist', 'dummyUser');
+--
+--  Sequence alter
+--
+alter sequence presentidea_seq restart with 6;
+alter sequence wishlist_seq restart with 3;
