1a8ed7518b3266b1c43b8f0f1384f29388d8c1da
Miggi <noreply@mymiggi.de>
2025-05-27 19:33:14 +0200

    🚧 Fix QuarkusIntegrationTest

--- src/test/java/com/github/presentnow/WishListResourceTest.java
+++ src/test/java/com/github/presentnow/WishListResourceTest.java
@@ -1,83 +1,56 @@
 package com.github.presentnow;
 
-import com.github.presentnow.db.WishListRepository;
 import com.github.presentnow.entity.WishList;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 @TestHTTPEndpoint(WishListResource.class)
 class WishListResourceTest
 {
-	public static final long NEW_TEST_ID = 3L;
-
-	@Inject
-	WishListRepository wishListRepository;
-
-	@AfterEach
-	@Transactional
-	void cleanUp()
-	{
-		wishListRepository.deleteById(NEW_TEST_ID);
-	}
-
 	@Test
 	void testCreateWishList()
 	{
 		WishList myList = getWishList();
 
-		Response response = given()
+		given()
 			.when()
 			.body(myList)
 			.contentType(ContentType.JSON)
-			.post();
-
-		response.then().statusCode(200);
-		WishList wishList = response.body().as(WishList.class);
-
-		assertEquals(NEW_TEST_ID, wishList.getId());
+			.post()
+			.then()
+			.statusCode(200)
+			.body("id", is(3));
 	}
 
 	@Test
 	void testGetWishListsByUser()
 	{
-		Response response = given()
+		given()
 			.when()
-			.get();
-
-		response.then().statusCode(200);
-		WishList[] wishList = response.body().as(WishList[].class);
-
-		int expectedLists = 2;
-		assertEquals(expectedLists, wishList.length);
+			.get()
+			.then()
+			.statusCode(200)
+			.body("size()", is(3));
 	}
 
 	@Test
 	void testGetWishListById()
 	{
-		long testId = 2;
-		String testTitle = "Birthday Gift Wishlist";
+		int testId = 2;
 
-		Response response = given()
+		given()
 			.when()
-			.get("/" + testId);
-
-		response.then().statusCode(200);
-		WishList wishList = response.body().as(WishList.class);
-
-		assertNotNull(wishList);
-		assertEquals(testTitle, wishList.getName());
-		assertEquals(testId, wishList.getId());
+			.get("/" + testId)
+			.then()
+			.statusCode(200)
+			.body("id", is(testId))
+			.body("name", is("Birthday Gift Wishlist"));
 	}
 
 	private static WishList getWishList()
