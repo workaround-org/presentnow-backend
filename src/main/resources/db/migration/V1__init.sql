3f1b6560764b7507b23180fe81c1eb56fb7fa9ae
Miggi <noreply@mymiggi.de>
2025-09-03 20:56:02 +0200

    🗃️ Add Flyway dependency and initialize PresentIdea and WishList tables

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE PresentIdea
+(
+    id          UUID    NOT NULL,
+    listId      BIGINT,
+    name        VARCHAR(255),
+    url         VARCHAR(255),
+    description VARCHAR(255),
+    importance  INTEGER NOT NULL,
+    CONSTRAINT pk_presentidea PRIMARY KEY (id)
+);
+
+CREATE TABLE WishList
+(
+    id          UUID NOT NULL,
+    name        VARCHAR(255),
+    description VARCHAR(255),
+    username    VARCHAR(255),
+    active      BOOLEAN,
+    expires     BIGINT,
+    CONSTRAINT pk_wishlist PRIMARY KEY (id)
+);
