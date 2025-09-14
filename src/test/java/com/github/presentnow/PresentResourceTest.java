eadbdf766026ece9c07d9b2d3249c3bdee046f00
Miggi <noreply@mymiggi.de>
2025-09-14 15:49:01 +0200

    🚧 (API): Add PresentResourceTest

--- src/test/java/com/github/presentnow/PresentResourceTest.java
+++ src/test/java/com/github/presentnow/PresentResourceTest.java
@@ -60,6 +60,70 @@ public class PresentResourceTest
 			.statusCode(200);
 	}
 
+	@Test
+	@TestTransaction
+	void updatePresent()
+	{
+		PresentIdea updatedPresent = new PresentIdea();
+		updatedPresent.setName("Updated Present Name");
+		updatedPresent.setDescription("Updated Description");
+		updatedPresent.setUrl("https://updated.example.com");
+		updatedPresent.setImportance(5);
+		updatedPresent.setListId(UUID.fromString("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"));
+
+		given()
+			.when()
+			.contentType(ContentType.JSON)
+			.body(updatedPresent)
+			.put("11111111-1111-1111-1111-111111111111")
+			.then()
+			.statusCode(200)
+			.body("name", is("Updated Present Name"))
+			.body("description", is("Updated Description"))
+			.body("url", is("https://updated.example.com"))
+			.body("importance", is(5));
+	}
+
+	@Test
+	@TestTransaction
+	void updatePresentNotFound()
+	{
+		PresentIdea updatedPresent = new PresentIdea();
+		updatedPresent.setName("Updated Present Name");
+		updatedPresent.setDescription("Updated Description");
+
+		given()
+			.when()
+			.contentType(ContentType.JSON)
+			.body(updatedPresent)
+			.put("ffffffff-ffff-ffff-ffff-ffffffffffff")
+			.then()
+			.statusCode(404);
+	}
+
+	@Test
+	@TestTransaction
+	void deletePresent()
+	{
+		// Delete the present
+		given()
+			.when()
+			.delete("11111111-1111-1111-1111-111111111111")
+			.then()
+			.statusCode(204);
+	}
+
+	@Test
+	@TestTransaction
+	void deletePresentNotFound()
+	{
+		given()
+			.when()
+			.delete("ffffffff-ffff-ffff-ffff-ffffffffffff")
+			.then()
+			.statusCode(404);
+	}
+
 	private PresentIdea getTestPresent()
 	{
 		PresentIdea presentIdea = new PresentIdea();
