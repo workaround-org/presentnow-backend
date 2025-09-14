b0d4789075f90db30ca9fec6699fb7a6f0377a1c
Miggi <noreply@mymiggi.de>
2025-09-14 15:44:02 +0200

    🚧 (API): Add WishListResourceTests

--- src/test/java/com/github/presentnow/WishListResourceTest.java
+++ src/test/java/com/github/presentnow/WishListResourceTest.java
@@ -1,6 +1,7 @@
 package com.github.presentnow;
 
 import com.github.presentnow.entity.WishList;
+import com.github.presentnow.entity.WishListUpdate;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
@@ -9,8 +10,8 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
-@QuarkusTest
 @TestHTTPEndpoint(WishListResource.class)
+@QuarkusTest
 class WishListResourceTest
 {
 	public static final int NEXT_ENTITY_ID = 3;
@@ -38,7 +39,7 @@ class WishListResourceTest
 			.get()
 			.then()
 			.statusCode(200)
-			.body("size()", is(ENTITIES_IN_DB));
+			.body("size()", is(2));
 	}
 
 	@Test
@@ -52,10 +53,127 @@ class WishListResourceTest
 			.then()
 			.statusCode(200)
 			.body("id", is(testId))
-			.body("name", is("Birthday Gift Wishlist"));
+			.body("name", is("Just Name Change"));
 	}
 
-	private static WishList getWishList()
+	@Test
+	void testUpdateWishList()
+	{
+		String testId = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb";
+		WishListUpdate updateData = new WishListUpdate(
+			"Updated Birthday Wishlist",
+			"Updated description for my birthday gifts",
+			null,
+			null,
+			null
+		);
+
+		given()
+			.when()
+			.body(updateData)
+			.contentType(ContentType.JSON)
+			.put("/" + testId)
+			.then()
+			.statusCode(204);
+	}
+
+	@Test
+	void testUpdateWishListNameOnly()
+	{
+		String testId = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb";
+		WishListUpdate updateData = new WishListUpdate(
+			"Just Name Change",
+			null,
+			null,
+			null,
+			null
+		);
+
+		given()
+			.when()
+			.body(updateData)
+			.contentType(ContentType.JSON)
+			.put("/" + testId)
+			.then()
+			.statusCode(204);
+	}
+
+	@Test
+	void testUpdateWishListDescriptionOnly()
+	{
+		String testId = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb";
+		WishListUpdate updateData = new WishListUpdate(
+			null,
+			"Just description change",
+			null,
+			null,
+			null
+		);
+
+		given()
+			.when()
+			.body(updateData)
+			.contentType(ContentType.JSON)
+			.put("/" + testId)
+			.then()
+			.statusCode(204);
+	}
+
+	@Test
+	void testUpdateNonExistentWishList()
+	{
+		String nonExistentId = "ffffffff-ffff-ffff-ffff-ffffffffffff";
+		WishListUpdate updateData = new WishListUpdate(
+			"Updated Name",
+			"Updated Description",
+			null,
+			null,
+			null
+		);
+
+		given()
+			.when()
+			.body(updateData)
+			.contentType(ContentType.JSON)
+			.put("/" + nonExistentId)
+			.then()
+			.statusCode(404);
+	}
+
+	@Test
+	void testDeleteWishList()
+	{
+		String testId = "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa";
+
+		// First verify the list exists
+		given()
+			.when()
+			.get("/" + testId)
+			.then()
+			.statusCode(200)
+			.body("id", is(testId));
+
+		// Delete the list
+		given()
+			.when()
+			.delete("/" + testId)
+			.then()
+			.statusCode(204);
+	}
+
+	@Test
+	void testDeleteNonExistentWishList()
+	{
+		String nonExistentId = "ffffffff-ffff-ffff-ffff-ffffffffffff";
+
+		given()
+			.when()
+			.delete("/" + nonExistentId)
+			.then()
+			.statusCode(404);
+	}
+
+	private WishList getWishList()
 	{
 		WishList myList = new WishList();
 		myList.setName("Test List");
