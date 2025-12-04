23de97ca1df45cd8f20b619233476c11f7607241
simonkoepf <207682734+simon-k3@users.noreply.github.com>
2025-12-04 15:08:27 +0100

    Add Present-Ideas Counter

--- src/main/java/com/github/presentnow/entity/ActiveWishList.java
+++ src/main/java/com/github/presentnow/entity/ActiveWishList.java
@@ -4,43 +4,43 @@ import java.util.UUID;
 
 public class ActiveWishList
 {
-private final UUID id;
-private final String name;
-private final String description;
-private final int presentIdeasCount;
-private final Boolean active;
+	private final UUID id;
+	private final String name;
+	private final String description;
+	private final int presentIdeasCount;
+	private final Boolean active;
 
-public ActiveWishList(WishList wishList)
-{
-this.id = wishList.getId();
-this.name = wishList.getName();
-this.description = wishList.getDescription();
-this.presentIdeasCount = wishList.getPresentIdeas() != null ? wishList.getPresentIdeas().size() : 0;
-this.active = wishList.getActive();
-}
+	public ActiveWishList(WishList wishList)
+	{
+		this.id = wishList.getId();
+		this.name = wishList.getName();
+		this.description = wishList.getDescription();
+		this.presentIdeasCount = wishList.getPresentIdeas() != null ? wishList.getPresentIdeas().size() : 0;
+		this.active = wishList.getActive();
+	}
 
-public UUID getId()
-{
-return id;
-}
+	public UUID getId()
+	{
+		return id;
+	}
 
-public String getName()
-{
-return name;
-}
+	public String getName()
+	{
+		return name;
+	}
 
-public String getDescription()
-{
-return description;
-}
+	public String getDescription()
+	{
+		return description;
+	}
 
-public int getPresentIdeasCount()
-{
-return presentIdeasCount;
-}
+	public int getPresentIdeasCount()
+	{
+		return presentIdeasCount;
+	}
 
-public Boolean getActive()
-{
-return active;
-}
+	public Boolean getActive()
+	{
+		return active;
+	}
 }
